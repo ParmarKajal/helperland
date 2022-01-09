6124c72060be4fa247914fd618c933f6589b8afb
Kajal Parmar <kajalparmar2131@gmail.com>
2022-01-10 00:04:28 +0530

    edit

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -12,3 +12,19 @@
           navbar.classList.remove("sticky");
       }
   } 
+
+
+  document.querySelector("#first-page").style.transform = "rotate(180deg)";
+  document.querySelector("#previous-page").style.transform = "rotate(180deg)";
+
+   function openNav() {
+   document.getElementById("mySidebar").style.width = "250px";
+   document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  }
+
+  document.querySelector("#next-page").style.transform = "rotate(180deg)";
